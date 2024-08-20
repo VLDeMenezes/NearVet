c2b6d5533ff59f382b22d052c2616ada28da7656
VLDeMenezes <estudiodemenezes@gmail.com>
2024-08-20 18:18:53 -0300

    Readaptacion del componente screen, margin y centrado

--- front/src/components/Screen.tsx
+++ front/src/components/Screen.tsx
@@ -3,7 +3,7 @@ interface ScreenProps {
 }
 
 const Screen: React.FC<ScreenProps> = ({ children }) => {
-  return (<main className="shadow-lg w-full lg:w-5/6 flex flex-col m-auto p-5 ">
+  return (<main className="shadow-lg w-full lg:w-5/6 flex flex-col mx-auto my-5 p-5 h-[100lvh] text-center justify-center rounded-md ">
     {children}
   </main>);
 };
