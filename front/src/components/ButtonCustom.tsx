f48680848ab98d409fb220cd5506f053e08971c8
VLDeMenezes <estudiodemenezes@gmail.com>
2024-08-20 22:17:33 -0300

    Improved CustomButton

--- front/src/components/ButtonCustom.tsx
+++ front/src/components/ButtonCustom.tsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
 
 interface ButtonCustomProps {
-  href?: string;
   text: string;
-  onClick?: () => void;
-  size?: string
 }
-const ButtonCustom: React.FC<ButtonCustomProps> = ({ href, text, onClick, size }) => {
+
+interface ButtonCustomOptionalProps extends ButtonCustomProps {
+  href: string;
+  size: string;
+  color: string;
+  onClick: () => void;
+
+}
+const ButtonCustom: React.FC<ButtonCustomProps & Partial<ButtonCustomOptionalProps>> = ({ href, text, onClick, size = 'base', color = 'white' }) => {
+  const sizeClass = `text-${size}`;
+  const colorClass = `text-${color}`;
+
   return (
-
     href ? (
-      <Link href={href} className={`bg-detail px-5 py-2 m-auto rounded-lg text-lg ${size}`}>{text}</Link>
-    ) :
-      <button onClick={onClick} className={`bg-detail px-5 py-2 m-auto rounded-lg text-lg ${size}`}>{text}</button>
-
+      <Link href={href} aria-label={`Link para ${text}`} className={`bg-detail px-5 py-2 m-auto rounded-lg text-lg ${sizeClass} ${colorClass}`}>{text}</Link>
+    ) : (
+      <button onClick={onClick} aria-label={`Boton para ${text}`} className={`bg-detail px-5 py-2 m-auto rounded-lg text-lg ${sizeClass} ${colorClass}`}>{text}</button>
+    )
   );
 };
+
 
 export default ButtonCustom;
